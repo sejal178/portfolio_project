fe42adb5054b1d0fb679f8aff5b36eca9e815e12
Sejal Vijay <sejalvijay0319@gmail.com>
2024-08-07 16:09:42 +0530

    ProjectPhotos

--- src/data.js
+++ src/data.js
@@ -1,22 +1,25 @@
 
 import Project11 from  './images/Project11.jpeg';
+import Project12 from  './images/Project12.png';
+import Project13 from  './images/Project13.png';
+
 export const webPortfolio = [
   {
     id: 1,
     title: "Web3 Crowdfunding App",
     link:"https://github.com/sejal178/Crowdfundingdapp_project",
-    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+    img: Project11,
   },
   {
     id: 2,
     title: "Netflix Clone",
-    img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+    img: Project12,
   },
   {
     id: 3,
     title: "Weather App",
     link:"https://github.com/sejal178/weatherApp",
-    img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+    img: Project13,
   },
   {
     id: 4,
@@ -34,8 +37,7 @@ export const webPortfolio = [
     id: 6,
     title: "Employee Management System",
     link:'https://github.com/sejal178/to-do-list',
-    img: Project11,
-    className:"w-[100px]"
+    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
   },
   
 ];
